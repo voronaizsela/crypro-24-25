8832c8b9a99647cf7a934b4910c4062238ae2339
dr34dm4tt3r <andrewhavyuk10@gmail.com>
2024-09-29 22:40:26 +0300

    nearly completed code...

--- tasks/cp1/khaviuk_fb-21_zakhozhyi_fb-21_cp1/lab1.py
+++ tasks/cp1/khaviuk_fb-21_zakhozhyi_fb-21_cp1/lab1.py
@@ -4,9 +4,13 @@ FILENAME_WITH_WHITESPACES = "./seneka.txt"
 FILENAME_WITHOUT_WHITESPACES = "./seneka_no_whitespace.txt"
 ALPHABET = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
 
-def prepare_text(text):
-    text_filtered = ''.join([char.lower() for char in text if char.lower() in ALPHABET])
-    text_filtered = ' '.join(text_filtered.split())
+def prepare_text(text, with_space=False):
+    if with_space==True:
+        text_filtered = ''.join([char.lower() for char in text if char.lower() in (ALPHABET+' ')])
+        text_filtered = ' '.join(text_filtered.split())
+    else:
+        text_filtered = ''.join([char.lower() for char in text if char.lower() in ALPHABET])
+        text_filtered = ' '.join(text_filtered.split())
     return text_filtered
 
 def calculate_monogram_frequencies(text_filtered):
@@ -27,7 +31,7 @@ def calculate_monogram_frequencies(text_filtered):
     return mono_freq_percentage
 
 def calculate_monogram_entropy(mono_freq_percentage):
-    mono_entropy = None
+    mono_entropy = 0
     for freq in mono_freq_percentage.values():
         if not freq:
             continue
@@ -47,17 +51,17 @@ def calculate_bigram_frequencies(text_filtered, step):
         bigram = text_filtered[i:i+2]
         bi_counter += 1
         if bigram in bi_freq_counter:
-            bi_freq_counter[monogram] += 1
+            bi_freq_counter[bigram] += 1
         else:
-            bi_freq_counter[monogram] = 1
+            bi_freq_counter[bigram] = 1
 
-    for monogram, freq in bi_freq_counter.items():
-        bi_freq_percentage[monogram] = freq/bi_counter
+    for bigram, freq in bi_freq_counter.items():
+        bi_freq_percentage[bigram] = freq/bi_counter
 
     return bi_freq_percentage
 
 def calculate_bigram_entropy(bi_freq_percentage):
-    bi_entropy = None
+    bi_entropy = 0
     for freq in bi_freq_percentage.values():
         if not freq:
             continue
@@ -71,11 +75,100 @@ def calculate_bigram_redundancy(bi_entropy):
 
 def main():
 
+    # working with file with whitespaces
     with open(FILENAME_WITH_WHITESPACES, 'r', encoding='utf-8') as file:
         text_with_whitespaces = file.read()
     
-    text_filtered = prepare_text(text_with_whitespaces)
+        # working with monograms
+        text_with_whitespaces_filtered = prepare_text(text_with_whitespaces, with_space=True)
+        mono_freq_percentage = calculate_monogram_frequencies(text_with_whitespaces_filtered)
+        H1_mono_entropy = calculate_monogram_entropy(mono_freq_percentage)
+        R1_mono_redundancy = calculate_monogram_redundancy(H1_mono_entropy)
+
+        # working with bigrams with step 1
+        bi_freq_percentage = calculate_bigram_frequencies(text_with_whitespaces_filtered, 1)
+        H2_bi_entropy_step1 = calculate_bigram_entropy(bi_freq_percentage)
+        R2_bi_redundancy_step1 = calculate_bigram_redundancy(H2_bi_entropy_step1)
+
+        # working with bigrams with step 2
+        bi_freq_percentage = calculate_bigram_frequencies(text_with_whitespaces_filtered, 2)
+        H2_bi_entropy_step2 = calculate_bigram_entropy(bi_freq_percentage)
+        R2_bi_redundancy_step2 = calculate_bigram_redundancy(H2_bi_entropy_step2)
+
+        # printing required info
+        print("\n^^^^ WORKING WITH FILE THAT HAS WHITESPACES ^^^^\n\n")
+
+        # Printing monogram frequencies line by line
+        print("Monogram frequencies:")
+        for monogram, frequency in mono_freq_percentage.items():
+            print(f"{monogram}: {frequency}")
+
+        print(f"\nMonogram's entropy: {H1_mono_entropy}\n")
+
+        # Printing bigram frequencies line by line (step 1)
+        print("Bigram frequencies (step 1, with whitespaces):")
+        for bigram, frequency in bi_freq_percentage.items():
+            print(f"{bigram}: {frequency}")
+
+        print(f"\nSTEP ONE Bigram's entropy: {H2_bi_entropy_step1}\n")
+        print(f"STEP ONE Bigram's redundancy: {R2_bi_redundancy_step1}\n")
+
+        # Printing bigram frequencies line by line (step 2)
+        print("Bigram frequencies (step 2, with whitespaces):")
+        for bigram, frequency in bi_freq_percentage.items():
+            print(f"{bigram}: {frequency}")
+
+        print(f"\nSTEP TWO Bigram's entropy: {H2_bi_entropy_step2}\n")
+        print(f"STEP TWO Bigram's redundancy: {R2_bi_redundancy_step2}\n")
+        print("-"*60)
+
+    # working with file withOUT whitespaces
+    with open(FILENAME_WITHOUT_WHITESPACES, 'r', encoding='utf-8') as file:
+        text_without_whitespaces = file.read()
     
+        # working with monograms
+        text_without_whitespaces_filtered = prepare_text(text_without_whitespaces)
+        mono_freq_percentage = calculate_monogram_frequencies(text_without_whitespaces_filtered)
+        H1_mono_entropy = calculate_monogram_entropy(mono_freq_percentage)
+        R1_mono_redundancy = calculate_monogram_redundancy(H1_mono_entropy)
+
+        # working with bigrams with step 1
+        bi_freq_percentage = calculate_bigram_frequencies(text_without_whitespaces_filtered, 1)
+        H2_bi_entropy_step1 = calculate_bigram_entropy(bi_freq_percentage)
+        R2_bi_redundancy_step1 = calculate_bigram_redundancy(H2_bi_entropy_step1)
+
+        # working with bigrams with step 2
+        bi_freq_percentage = calculate_bigram_frequencies(text_without_whitespaces_filtered, 2)
+        H2_bi_entropy_step2 = calculate_bigram_entropy(bi_freq_percentage)
+        R2_bi_redundancy_step2 = calculate_bigram_redundancy(H2_bi_entropy_step2)
+
+        # printing required info
+        print("\n^^^^ WORKING WITH FILE THAT HAS >NOT< WHITESPACES ^^^^\n\n")
+
+        # Printing monogram frequencies line by line
+        print("Monogram frequencies (without whitespaces):")
+        for monogram, frequency in mono_freq_percentage.items():
+            print(f"{monogram}: {frequency}")
+
+        print(f"\nMonogram's entropy: {H1_mono_entropy}\n")
+
+        # Printing bigram frequencies line by line (step 1)
+        print("Bigram frequencies (step 1, without whitespaces):")
+        for bigram, frequency in bi_freq_percentage.items():
+            print(f"{bigram}: {frequency}")
+
+        print(f"\nSTEP ONE Bigram's entropy: {H2_bi_entropy_step1}\n")
+        print(f"STEP ONE Bigram's redundancy: {R2_bi_redundancy_step1}\n")
+
+        # Printing bigram frequencies line by line (step 2)
+        print("Bigram frequencies (step 2, without whitespaces):")
+        for bigram, frequency in bi_freq_percentage.items():
+            print(f"{bigram}: {frequency}")
+
+        print(f"\nSTEP TWO Bigram's entropy: {H2_bi_entropy_step2}\n")
+        print(f"STEP TWO Bigram's redundancy: {R2_bi_redundancy_step2}\n")
+
+    return
 
 if __name__ == "__main__":
     main()
