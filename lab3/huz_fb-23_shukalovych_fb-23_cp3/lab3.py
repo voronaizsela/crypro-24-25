722f7ed0443e60eb978326d52c6269c322042e85
moki-vi <viktorguz-ipt27@lll.kpi.ua>
2024-10-26 12:11:28 +0300

    modified-bigram-functions

--- lab3/huz_fb-23_shukalovych_fb-23_cp3/lab3.py
+++ lab3/huz_fb-23_shukalovych_fb-23_cp3/lab3.py
@@ -1,14 +1,36 @@
-import sys
-import os
-
-sys.path.append(r"C:\study\3 course\crypto\crypro-24-25\lab1\huz_fb-23_shukalovych_fb-23_cp1")
-
-from lab1 import * 
 from math_operations import *
+from collections import Counter
 
 YELLOW = "\033[93m"
 BLUE = "\033[94m"
 RESET = "\033[0m"
+
+#кількість біграм без перетину
+def count_bigrams_no_overlap(text):
+    bigrams = [text[i:i + 2] for i in range(0, len(text) - 1, 2)]
+    bigram_counts = Counter(bigrams)
+    return bigram_counts
+
+#загальна кількість біграм
+def count_total_bigrams(bigram_counts):
+    total_bigrams = sum(bigram_counts.values())
+    return total_bigrams
+
+#частота біграм
+def bigram_frequencies(bigram_counts, total_bigrams):
+    frequencies = {}
+    for bigram, count in bigram_counts.items():
+        frequencies[bigram] = count / total_bigrams
+    return frequencies
+
+def print_top_5_bigrams(bigram_frequencies):
+    top_5_bigrams = dict(sorted(bigram_frequencies.items(), key=lambda x: x[1], reverse=True)[:5])
+
+    print("Найчастіші біграми та їх частоти:")
+    for bigram, frequency in top_5_bigrams.items():
+        print(f"{bigram}: {frequency:.5f}")
+
+    return top_5_bigrams
 
 def main():
     while True:
@@ -76,16 +98,19 @@ def main():
                 else:
                     print("Неправильний вибір. Спробуйте знову.")
         elif user_choice == '2':
-            bigram_counts = count_bigrams_no_overlap("04.txt")
+            with open('04.txt', 'r', encoding='utf-8') as file:
+                content = file.read()
+            bigram_counts = count_bigrams_no_overlap(content)
             total_bigrams = count_total_bigrams(bigram_counts)
-            bigram_frequenc = bigram_frequencies(bigram_counts, total_bigrams)
-            print_bigram_frequencies(bigram_frequenc)
+            bigram_freq = bigram_frequencies(bigram_counts, total_bigrams)
+            #print(bigram_freq)
+            print_top_5_bigrams(bigram_freq)
         elif user_choice == '3':
+            print("краказябра")
+        elif user_choice == '4':
             print("краказябра")
         else:
             print("Неправильний вибір. Спробуйте знову.")
 
-
 if __name__ == "__main__":
-
     main()
